09cc3bfafc3506b88d86b11c5b5d4637e8e32f70
Haluk Dogan <hlk.dogan@gmail.com>
2025-04-12 18:41:40 -0400

    Update post_install.sh

--- home_config/post_install.sh
+++ home_config/post_install.sh
@@ -3,3 +3,10 @@ yay -S ttf-symbola slack-desktop zoom
 sudo sed -i 's/^#\(export FREETYPE_PROPERTIES="truetype:interpreter-version=40"\)/\1/' /etc/profile.d/freetype2.sh
 
 fc-cache -fv
+
+# docker
+sudo pacman -S docker docker-compose
+sudo systemctl start docker
+sudo groupadd docker
+sudo gpasswd -a $USER docker
+newgrp docker
