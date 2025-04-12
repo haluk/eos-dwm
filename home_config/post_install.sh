6df3240d5178b6a8f95306fb81178e9c4b3db066
Haluk Dogan <hlk.dogan@gmail.com>
2025-04-12 00:57:07 -0400

    Update post_install.sh

--- home_config/post_install.sh
+++ home_config/post_install.sh
@@ -3,3 +3,6 @@ yay -S ttf-symbola slack-desktop zoom
 sudo sed -i 's/^#\(export FREETYPE_PROPERTIES="truetype:interpreter-version=40"\)/\1/' /etc/profile.d/freetype2.sh
 
 fc-cache -fv
+
+chsh -s /bin/zsh
+echo 'eval "$(starship init zsh)"' >> ~/.zshrc
