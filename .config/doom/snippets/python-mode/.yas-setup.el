e74085a8188f82eac5a991f476e1b3032ce264d8
Haluk Dogan <hlk.dogan@gmail.com>
2025-06-04 01:10:16 -0400

    doom snippet and new packages

---
+++ .config/doom/snippets/python-mode/.yas-setup.el
@@ -0,0 +1,15 @@
+(require 'yasnippet)
+(defvar yas-text)
+
+(defun python-args-to-docstring-numpy ()
+  "return docstring format for the python arguments in yas-text"
+  (let* ((args (python-split-args yas-text))
+         (format-arg (lambda(arg)
+                       (concat (nth 0 arg) " : " (if (nth 1 arg) ", optional") "\n")))
+         (formatted-params (mapconcat format-arg args "\n"))
+         (formatted-ret (mapconcat format-arg (list (list "out")) "\n")))
+    (unless (string= formatted-params "")
+      (mapconcat 'identity
+                 (list "\nParameters\n----------" formatted-params
+                       "\nReturns\n-------" formatted-ret)
+                 "\n"))))
