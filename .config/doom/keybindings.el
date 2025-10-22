2686a854ee41c54462171e1f4079e830380714a7
Haluk Dogan <hlk.dogan@gmail.com>
2025-10-22 12:43:13 -0400

    Update keybindings.el

--- .config/doom/keybindings.el
+++ .config/doom/keybindings.el
@@ -2,4 +2,6 @@
 
 (map! :nv "C-c t" 'treemacs-select-window)
 (map! :i "M-<backspace>" 'sp-backward-delete-word)
-
+(map! :leader
+      :desc "Yank visible text only"
+      "oy" #'evil-yank-visible)
